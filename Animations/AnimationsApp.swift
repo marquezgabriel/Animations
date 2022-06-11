2cd87a1744111c925453c14199c733662e046653
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-11 13:37:30 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Gabriel Marquez on 2022-06-11.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
